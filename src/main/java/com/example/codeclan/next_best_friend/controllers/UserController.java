4835388bab236dcd40695cdabaa149198e8ecc12
Ruairidh Benentt <ruairidhbennett@gmail.com>
2020-07-12 11:11:06 +0100

    floobits restart

--- src/main/java/com/example/codeclan/next_best_friend/controllers/UserController.java
+++ src/main/java/com/example/codeclan/next_best_friend/controllers/UserController.java
@@ -1,16 +1,15 @@
 package com.example.codeclan.next_best_friend.controllers;
 
+import com.example.codeclan.next_best_friend.models.Breeder;
 import com.example.codeclan.next_best_friend.models.User;
+import com.example.codeclan.next_best_friend.repositories.BreederRepository;
 import com.example.codeclan.next_best_friend.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -19,6 +18,9 @@ public class UserController {
 
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    BreederRepository breederRepository;
 
     @Autowired
     private PasswordEncoder passwordEncoder;
@@ -40,17 +42,46 @@ public class UserController {
         return new ResponseEntity<>(foundUser, HttpStatus.OK);
     }
 
-    @PostMapping(value = "/register")
+    @PostMapping(value = "/register/user")
     public ResponseEntity registerUser(@RequestBody User user){
         Optional<User> foundUser = userRepository.findByUsername(user.getUsername());
         if (foundUser.isPresent()){
             return new ResponseEntity("That Username Already Exists", HttpStatus.BAD_REQUEST);
         }
+
         String password = passwordEncoder.encode(user.getPassword());
         user.setPassword(password);
+        user.setRoles("ROLE_USER");
         userRepository.save(user);
         return new ResponseEntity("Reg Success", HttpStatus.OK);
+
     }
+
+    @PostMapping(value = "/register/breeder")
+    public ResponseEntity registerBreeder(@RequestBody Breeder breeder){
+        Optional<User> foundUser = userRepository.findByUsername(breeder.getUsername());
+        Optional<Breeder> foundBreeder = breederRepository.findByUsername(breeder.getUsername());
+        System.out.println(breeder);
+        if (foundUser.isPresent()){
+            return new ResponseEntity("That Username Already Exists", HttpStatus.BAD_REQUEST);
+        }
+
+        if (breeder.getBreederNumber() == null) {
+            return new ResponseEntity("Please Enter Breeder Number", HttpStatus.BAD_REQUEST);
+        }
+        String password = passwordEncoder.encode(breeder.getPassword());
+        breeder.setPassword(password);
+        breeder.setRoles("ROLE_BREEDER");
+        breederRepository.save(breeder);
+        return new ResponseEntity("Welcome Breeder", HttpStatus.OK);
+
+    }
+
+    @DeleteMapping(value = "/user/{id}"){
+
+    }
+
+
 
 
 
--- src/main/java/com/example/codeclan/next_best_friend/repositories/BreederRepository.java
+++ src/main/java/com/example/codeclan/next_best_friend/repositories/BreederRepository.java
@@ -1,9 +1,15 @@
 package com.example.codeclan.next_best_friend.repositories;
 
 import com.example.codeclan.next_best_friend.models.Breeder;
+import com.example.codeclan.next_best_friend.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BreederRepository extends JpaRepository<Breeder, Long> {
+    Optional<Breeder> findByUsername(String username);
+
+    Boolean findByBreederNumberNotNull();
 }
